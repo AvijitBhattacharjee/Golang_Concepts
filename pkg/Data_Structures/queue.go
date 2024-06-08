c41b90f416a30aabe9916660f68c27cec863770b
Avijit Bhattacharjee <avijit@avijit.myguest.virtualbox.org>
2024-06-08 18:46:21 +0530

    adding stack, queue, single linked list, double linked list

--- pkg/Data_Structures/queue.go
+++ pkg/Data_Structures/queue.go
@@ -1,3 +1,48 @@
 // Copyright (c) avijit bhattacharjee 2024
 
 package Data_Structures
+
+
+import ( 
+	"fmt"
+)
+
+type MyQueue struct {
+	myslice []interface{}
+}
+
+func pushQueue(myqueue *MyQueue, val int) {
+
+	myqueue.myslice = append(myqueue.myslice, val)
+	fmt.Println(val, " has been pushed")
+}
+
+func popQueue(myqueue *MyQueue) {
+	
+	val := myqueue.myslice[0]
+	myqueue.myslice = myqueue.myslice[1:]
+	fmt.Println(val, " has been popped")
+}
+
+func displayQueue(myqueue *MyQueue) {
+	
+	queueLenght := len(myqueue.myslice)
+
+	for i := 0; i < queueLenght; i++ {
+		fmt.Print(myqueue.myslice[i], " ")
+	}
+	fmt.Println(" ")
+}
+
+func ImplementQueue() {
+
+	var myqueue = &MyQueue{}
+	pushQueue(myqueue, 11)
+	pushQueue(myqueue, 22)
+	pushQueue(myqueue, 33)
+
+	displayQueue(myqueue)
+
+	popQueue(myqueue)
+	displayQueue(myqueue)
+}
--- pkg/Data_Structures/singleLinkedList.go
+++ pkg/Data_Structures/singleLinkedList.go
@@ -1,3 +1,100 @@
 // Copyright (c) avijit bhattacharjee 2024
 
 package Data_Structures
+
+import (
+	"fmt"
+)
+
+type SingleNode struct {
+	Val  int
+	Next *SingleNode
+}
+
+func insertStartSLL(root *SingleNode, val int) *SingleNode {
+	var newSingleNode = &SingleNode{
+		Val:  val,
+		Next: root,
+	}
+
+	fmt.Println(newSingleNode.Val, " has been added")
+	return newSingleNode
+}
+
+func insertEndSLL(root *SingleNode, val int) *SingleNode {
+	var newSingleNode = &SingleNode{
+		Val:  val,
+		Next: nil,
+	}
+
+	if root == nil {
+		return newSingleNode
+	}
+
+	current := root
+	for current.Next != nil {
+		current = current.Next
+	}
+	current.Next = newSingleNode
+
+	fmt.Println(newSingleNode.Val, " has been added")
+	return root
+}
+
+func deleteStartSLL(root *SingleNode) *SingleNode {
+	if root == nil {
+		return nil
+	}
+
+	fmt.Println(root.Val, " has been deleted")
+	return root.Next
+}
+
+func deleteEndSLL(root *SingleNode) *SingleNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Next == nil {
+		return nil
+	}
+
+	current := root
+	for current.Next.Next != nil {
+		current = current.Next
+	}
+	fmt.Println(current.Next.Val, " has been deleted")
+	current.Next = nil
+	return root
+}
+
+func displaySLL(root *SingleNode) {
+	if root == nil {
+		fmt.Println("nothing to show")
+		return
+	}
+
+	fmt.Println("Displaying Single Linked List")
+	for root != nil {
+		fmt.Print(root.Val, " ")
+		root = root.Next
+	}
+	fmt.Println("\nDisplayed Single Linked List")
+}
+
+func ImplementSingleLinkedList() {
+	var root *SingleNode
+
+	root = insertStartSLL(root, 3)
+	root = insertStartSLL(root, 2)
+	root = insertStartSLL(root, 1)
+	root = insertEndSLL(root, 4)
+	root = insertEndSLL(root, 5)
+
+	displaySLL(root)
+
+	root = deleteStartSLL(root)
+	root = deleteEndSLL(root)
+
+	displaySLL(root)
+}
