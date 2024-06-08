27d4f771a7488f746ea77e80abefe5b334434d74
Avijit Bhattacharjee <avijit@avijit.myguest.virtualbox.org>
2024-06-08 14:31:30 +0530

    adding all test cases

--- pkg/Rest_API/restapi.go
+++ pkg/Rest_API/restapi.go
@@ -1,25 +1,50 @@
+// Copyright (c) avijit bhattacharjee 2024
+
 package Rest_API
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"time"
+
 	"github.com/gorilla/mux"
 )
 
-
-
 func Start() {
-	
 	fmt.Println("Implementing simple rest API")
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", home)
 	router.HandleFunc("/{param1}/{param2}", paramsModify)
-	
-	err := http.ListenAndServe(":8080", router)
-	if err != nil {
-		fmt.Println(err)
+
+	// Create a server with timeouts
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  30 * time.Second,
 	}
+
+	// Run the server in a goroutine
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Printf("ListenAndServe(): %v\n", err)
+		}
+	}()
+
+	// Wait for a signal or shutdown trigger
+	// Here, simulate a shutdown after a delay for demonstration purposes
+	time.Sleep(10 * time.Second)
+
+	// Shutdown the server gracefully
+	fmt.Println("Shutting down the server...")
+	if err := srv.Shutdown(context.Background()); err != nil {
+		fmt.Printf("Server Shutdown Failed:%+v", err)
+	}
+
+	fmt.Println("Server exited properly")
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -31,5 +56,4 @@ func paramsModify(w http.ResponseWriter, r *http.Request) {
 	name := params["param1"]
 	age := params["param2"]
 	fmt.Println("These are the two parameters", name, age)
-	
 }
